00b49347824adcb535ca2372716b89901bb60e98
lorenzomaiuri-dev <maiurilorenzo@gmail.com>
2026-02-01 14:35:18 +0100

    chore: added more shadcn components

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,6 +9,12 @@ const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(file
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(dirname, './src'),
+      '$lib': path.resolve(dirname, './src/lib'),
+    },
+  },
   test: {
     environment: 'jsdom',
     setupFiles: ['./vitest-setup.ts'],

095fc6c8a068d313c666e4f1d9b93543bf91e3c2
lorenzomaiuri-dev <maiurilorenzo@gmail.com>
2026-02-07 12:27:33 +0100

    feat: added pipeline

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://lorenzomaiuri-dev.github.io',
+  base: '/marketing-engine-ai',
   integrations: [svelte()],
 
   vite: {
